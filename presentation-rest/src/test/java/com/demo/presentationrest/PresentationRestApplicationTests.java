1698386de2561055e4a43ad4a04d9678563fe4dc
minkee mac <minkeemac@minkeeui-MacBookPro.local>
2023-06-28 01:23:55 +0900

    feat: test settings revised

--- presentation-rest/src/test/java/com/demo/presentationrest/PresentationRestApplicationTests.java
+++ presentation-rest/src/test/java/com/demo/presentationrest/PresentationRestApplicationTests.java
@@ -1,9 +1,11 @@
 package com.demo.presentationrest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PresentationRestApplicationTests {
 
 	@Test
