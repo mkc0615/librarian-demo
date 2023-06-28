228e942329941a6a8bb1771ef94ac345e9fc5da1
minkee mac <minkeemac@minkeeui-MacBookPro.local>
2023-06-29 07:38:33 +0900

    feat: basic spring boot and db settings

--- presentation-rest/src/main/java/com/demo/presentationrest/PresentationRestApplication.java
+++ presentation-rest/src/main/java/com/demo/presentationrest/PresentationRestApplication.java
@@ -3,8 +3,9 @@ package com.demo.presentationrest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.demo.library")
 public class PresentationRestApplication {
 
 	public static void main(String[] args) {
